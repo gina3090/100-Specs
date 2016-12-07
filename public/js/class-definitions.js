ea3e500a8a1d65fe89cf4d5fecccd82a9e4d989f
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-12-07 10:23:49 -1000

    completed step 85

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1225,7 +1225,15 @@ Box.prototype.openBox = function() {
  * Return true if openClose opens the door, false if openClose closes the door.
  *
  */
-
+Door.prototype.openClose = function() {
+  if(this.isOpen === false) {
+    this.isOpen = true;
+    return true;
+  } else if(this.isOpen === true) {
+    this.isOpen = false;
+    return false;
+  }
+};
 
 /* Step 86
  *
