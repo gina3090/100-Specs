6056c67700412f49ae3504b3ee88f3dc9a7e8b86
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-12-07 09:08:39 -1000

    completed step 57

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1037,7 +1037,11 @@ class House {
  * Lightbulb class
  * @param {boolean} isOn Whether the light is on or off
  */
-
+class Lightbulb {
+  constructor(isOn) {
+    this.isOn = isOn;
+  }
+}
 
 /**
  * Step 58
