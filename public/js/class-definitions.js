283a70310f42ef8fec5b9c5e6a365ee34f4c8a5e
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-12-07 09:29:16 -1000

    completed step 67

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1087,7 +1087,7 @@ var hexagon = new Shape(6);
 
 // Create 2 boxes
 var catBox = new Box(new Animal('Cat'), true);
-var christmasPresent;
+var christmasPresent = new Box('iPad', false);
 
 // Create 2 doors
 var automaticDoor;
