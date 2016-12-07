d18d771bf2ee27778d64ce79fca77dea99ad3dca
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-12-06 23:30:46 -1000

    completed step 34

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -663,7 +663,18 @@ PrincessLeia.prototype.marries = function(loveInterest) {
  *   staplePapers
  *
  */
+function Stapler(color, maxPapers) {
+  this.color = color;
+  this.maxPapers = maxPapers;
+}
 
+Stapler.prototype.staplePapers = function(papers) {
+  if(papers <= this.maxPapers) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 /* Step 35
  *
