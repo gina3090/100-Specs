aed10acf8a294d7e0fc5d29c7eb811874ae4ab39
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-12-07 09:09:37 -1000

    completed step 58

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1049,7 +1049,11 @@ class Lightbulb {
  * Cookie class
  * @param {string} flavor The cookie's flavor
  */
-
+class Cookie {
+  constructor(flavor) {
+    this.flavor = flavor;
+  }
+}
 
 /**
  * Step 59
