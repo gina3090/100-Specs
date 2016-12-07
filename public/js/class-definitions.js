20a8bf6c09678650f0949fb0f1349bff6a6c02ff
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-12-07 09:31:38 -1000

    completed step 69

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1091,7 +1091,7 @@ var christmasPresent = new Box('iPad', false);
 
 // Create 2 doors
 var automaticDoor = new Door(true);
-var bankVault;
+var bankVault = new Door(false);
 
 // Create 2 shoes
 var rubySlippers;
